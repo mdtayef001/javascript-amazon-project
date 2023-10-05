e42bbb6e2f049622fc2373a10da11fb8d10f878b
Tayef <tayefhossain200@gmail.com>
2023-10-05 18:59:07 +0600

    fix price formatting and radio selectors

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 import { cart, addTocart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrrency } from "./utils/money.js";
 
 let productsHTML = "";
 
@@ -25,7 +26,7 @@ products.forEach((product) => {
         }</div>
     </div>
 
-    <div class="product-price">$${(product.priceCents / 100).toFixed(2)}</div>
+    <div class="product-price">$${formatCurrrency(product.priceCents)}</div>
 
     <div class="product-quantity-container">
         <select class="js-quantity-selector-${product.id}">
